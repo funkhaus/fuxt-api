dae04b63534ce2aa1cb7741469981ef269ef5941
Chengmin <chengmin.lee@breadstack.com>
2025-01-14 00:56:08 -0500

    reverted lux-v's change

--- includes/utils/class-acf.php
+++ includes/utils/class-acf.php
@@ -118,7 +118,7 @@ class Acf {
 								if ( $field['value'] ) {
 									foreach ( $field['value'] as $row ) {
 										foreach ( $sub_fields as $sub_field_name => &$sub_field ) {
-											$sub_field['value'] = $row[ $sub_field_name ] ?? $row[ $sub_field['key'] ] ?? null;
+											$sub_field['value'] = $row[ $sub_field_name ] ?? null;
 										}
 										$value[] = self::get_data_by_fields( $sub_fields );
 									}
